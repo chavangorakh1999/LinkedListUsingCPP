0cc7fc329904ac0918b90a8ad0c2fb316c6b1c5e
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-04-10 17:18:52 +0530

    Counting and adding Linked List Elements

---
+++ CountAndSumOfLinkedList/CountAndSumOfLinkedList/main.cpp
@@ -0,0 +1,99 @@
+//
+//  main.cpp
+//  CountAndSumOfLinkedList
+//
+//  Created by Gorakh Chavan on 10/04/20.
+//  Copyright © 2020 Gorakh Chavan. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int DATA;
+    struct Node *next;
+}*head;
+void create(int a[],int n)
+{
+    int i;
+    struct Node *NewNode,*LastPtr=NULL;
+    head=new Node;
+    head->DATA=a[0];
+    head->next=NULL;
+    LastPtr=head;
+    for(i=1;i<n;i++)
+    {
+        NewNode=new Node;
+        NewNode->DATA=a[i];
+        NewNode->next=NULL;
+        LastPtr->next=NewNode;
+        LastPtr=NewNode;
+        
+        
+    }
+}
+void display(struct Node *TempPtr)
+{
+    while(TempPtr!=NULL)
+    {
+        cout<<TempPtr->DATA<<" ";
+        TempPtr=TempPtr->next;
+        
+    }
+}
+//Iterative version of counting linkedlist
+int Count(struct Node *TempPtr)
+{
+    int count=0;
+    while(TempPtr)
+    {
+        count++;
+        TempPtr=TempPtr->next;
+    }
+    return count;
+}
+
+//Recursive function of counting elements
+int Rcount(struct Node *TempPtr)
+{
+    if(TempPtr)
+    {
+        return Rcount(TempPtr->next)+1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+//Sum using Iterative version
+int Add(struct Node *TempPtr)
+{
+    int sum=0;
+    while(TempPtr)
+    {
+        sum+=TempPtr->DATA;
+        TempPtr=TempPtr->next;
+    }
+    return sum;
+}
+//Recursive Function for Adding LL elements
+int Radd(struct Node *TempPtr)
+{
+    if(TempPtr)
+    {
+        return Radd(TempPtr->next)+TempPtr->DATA;
+    }
+    else{
+        return 0;
+    }
+}
+int main()
+{
+    int a[]={1,2,3,4,5};
+    create(a,5);
+    display(head);
+    cout<<endl<<Rcount(head)<<endl;
+    cout<<Add(head)<<endl;
+    cout<<Radd(head)<<endl;
+    
+    return 0;
+}
